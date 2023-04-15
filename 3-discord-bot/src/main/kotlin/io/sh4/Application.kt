29db2627bd9565907615498f779a87b9df3ac960
sh4 <krzysztof.wydrzynski99@gmail.com>
2023-04-15 14:24:22 +0200

    Token removed

--- 3-discord-bot/src/main/kotlin/io/sh4/Application.kt
+++ 3-discord-bot/src/main/kotlin/io/sh4/Application.kt
@@ -1,6 +1,6 @@
 package io.sh4
 
 fun main() {
-    val bot = DiscordBot("MTA5NjAwNDk4NDY3NzYxNzcyNA.G6zCr0.i2LdW3hyIGEDoMwmqC779J4GMhR5r6up__jqzo")
+    val bot = DiscordBot("xxx")
     bot.start()
 }
